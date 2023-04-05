ea87335a21c0ee0e59c36ac806700e9953438e96
Zhitikhina Mariia <zhitikhinamv@gmail.com>
2023-04-05 21:23:22 +0300

    Initial commit

---
+++ 2.3.1/2.3.1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+class Calculator {
+private:
+    double num1;
+    double num2;
+public:
+    double add() {
+        return num1 + num2;
+    }
+    double multiply() {
+        return num1 * num2;
+    }
+    double subtract_1_2() {
+        return num2 - num1;
+    }
+    double subtract_2_1() {
+        return num1 - num2;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
+    }
+    bool set_num1(double num) {
+        if (num != 0) {
+            num1 = num;
+            return true;
+        }
+        else return false;
+    }
+    bool set_num2(double num) {
+        if (num != 0) {
+            num2 = num;
+            return true;
+        }
+        else return false;
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int num1;
+    int num2;
+    bool i{ false };
+    bool j{ false };
+    Calculator calc;
+    while (true) {
+        if (!i) {
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+            i = calc.set_num1(num1);
+        }
+        if (!j) {
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+            j = calc.set_num2(num2);
+        }
+            if (i && j) {
+                std::cout << "num1 + num2 = " << calc.add() << std::endl;
+                std::cout << "num1 - num2 = " << calc.subtract_2_1() << std::endl;
+                std::cout << "num2 - num1 = " << calc.subtract_1_2() << std::endl;
+                std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+                std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+                std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+                i = false;
+                j = false;
+            }
+            else
+                std::cout << "Неверный ввод!" << std::endl;
+    }
+}
